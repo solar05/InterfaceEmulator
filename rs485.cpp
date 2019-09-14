d61d581c091a4336932902efc1fb3149edb91eb5
solar05 <artem00298@gmail.com>
2019-09-14 11:55:27 +0300

    Add port enabling and disabling

--- rs485.cpp
+++ rs485.cpp
@@ -1,7 +1,7 @@
 #include "rs485.h"
 #include "ui_rs485.h"
-#include <QSerialPort>
-#include <QSerialPortInfo>
+#include <QDebug>
+
 
 RS485::RS485(QWidget *parent) :
     QWidget(parent),
@@ -20,5 +20,43 @@ RS485::~RS485()
 
 void RS485::on_pushButton_clicked()
 {
+    port.close();
+    if (port.isOpen()) {
+        qDebug() << "port is open";
+    } else {
+        qDebug() << "port is closed";
+    }
     this->close();
+}
+
+void RS485::on_pushButton_2_clicked()
+{
+    QSerialPort::DataBits dataBits;
+    QSerialPort::Parity parity;
+    QSerialPort::StopBits stopBits;
+    QSerialPort::FlowControl flowControl;
+    QString portChoice = ui->listWidget->currentItem()->text();
+    qDebug() << portChoice;
+    port.setPortName(portChoice);
+    port.open(QIODevice::ReadWrite);
+    port.setBaudRate(QSerialPort::Baud9600);
+    port.setDataBits(dataBits);
+    port.setParity(parity);
+    port.setStopBits(stopBits);
+    port.setFlowControl(flowControl);
+    if (port.isOpen()) {
+        qDebug() << "port is open";
+    } else {
+        qDebug() << "port is closed";
+    }
+}
+
+void RS485::on_pushButton_3_clicked()
+{
+    port.close();
+    if (port.isOpen()) {
+        qDebug() << "port is open";
+    } else {
+        qDebug() << "port is closed";
+    }
 }
--- rs485.h
+++ rs485.h
@@ -2,6 +2,8 @@
 #define RS485_H
 
 #include <QWidget>
+#include <QSerialPort>
+#include <QSerialPortInfo>
 
 namespace Ui {
 class RS485;
@@ -14,9 +16,14 @@ class RS485 : public QWidget
 public:
     explicit RS485(QWidget *parent = nullptr);
     ~RS485();
+    QSerialPort port;
 
 private slots:
     void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::RS485 *ui;
